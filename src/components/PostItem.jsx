94e12b5776b4979e6ae65bd9f39595ef44f22599
CAPT <_capt_@mail.ru>
2023-01-14 12:34:19 +0300

    +1

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>1. ddfdf</strong>
+                <div>asdsad</div>
+            </div>
+            <div className="post__btns">
+                <button>Удалить</button>
+            </div>
+        </div>
+    )
+}
+
+export default PostItem
